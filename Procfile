4093f0e7333afe1b285289eb6359c0d4d248a90c
sharmaprinceji <princesh1411@gmail.com>
2026-02-02 21:07:57 +0530

    Add gunicorn and Procfile
    Signed-off-by: sharmaprinceji <princesh1411@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hrms_project.wsgi
--- hrms_project/settings.py
+++ hrms_project/settings.py
@@ -87,12 +87,18 @@ WSGI_APPLICATION = 'hrms_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.dummy",
     }
 }
+
 
 
 # Password validation
